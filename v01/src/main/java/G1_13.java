0100de870ce325e0daad11ab3df66992d812af8d
gcl1991 <737720583@qq.com>
2020-04-02 07:59:29 +0800

    Complete Exercise 1.13
    Implemented as an internal static class Collection,Implementation methods include:
     - isEmpty
     - makeEmpty
     - insert
     - remove
     - isPresent
     - getSize

--- v01/src/main/java/G1_13.java
+++ v01/src/main/java/G1_13.java
@@ -1,4 +1,82 @@
-package PACKAGE_NAME;
+// import lombok.Data;
 
 public class G1_13 {
+//    @Data
+    public static class Collection<T>{
+        private Object[] array;
+        public final int length;
+        private int size;
+
+        public Collection (int arraySize){
+            this.size = 0;
+            this.array = new Object[arraySize];
+            this.length = arraySize;
+        }
+        public boolean isEmpty (){
+            return size==0;
+        }
+
+        public void makEmpty (){
+            size = 0;
+            array = new Object[array.length];
+        }
+
+        public void insert (int index,T value) throws ArrayIndexOutOfBoundsException {
+            if(index<0 || index>=length){
+                throw new ArrayIndexOutOfBoundsException();
+            }
+            else if(size == length){
+                throw new ArrayFullException();
+            }
+            else if(index>=size){
+                array[index] = value;
+                size+=1;
+            }
+            else {
+                for (int i = size; i>index; i--){
+                    array[i] = array[i-1];
+                }
+                array[index] = value;
+                size+=1;
+            }
+        }
+
+        public T remove (int index) throws ArrayIndexOutOfBoundsException{
+            if(index<0 || index>=size)
+                throw new ArrayIndexOutOfBoundsException();
+            else{
+                T element = (T) array[index];
+                for (int i = index; i<size; i++){
+                    array[i] = array[i+1];
+                }
+                size-=1;
+                return element;
+            }
+
+        }
+
+        public boolean isPresent (T other){
+            // 非空性
+            if (other == null)
+                return false;
+            // 内容比较
+            for (int index=0;index<size;index++){
+                T theElement = (T) array[index];
+                if (theElement.equals(other))
+                    return true;
+            }
+            return false;
+        }
+
+        public int getSize (){
+            return size;
+        }
+    }
+}
+
+class ArrayFullException extends ArrayStoreException{
+    public ArrayFullException(){};
+    public ArrayFullException(String gripe){
+        super(gripe);
+    }
 }
--- v01/src/test/java/G1_13Test.java
+++ v01/src/test/java/G1_13Test.java
@@ -1,4 +1,38 @@
-package PACKAGE_NAME;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
 
 public class G1_13Test {
+    public G1_13.Collection<String> collection;
+    public G1_13Test(){
+        collection = new G1_13.Collection<>(10);
+        collection.insert(0,"1");
+        collection.insert(1,"2");
+    }
+    @Test
+    public void isEmptyTest(){
+        collection.makEmpty();
+        assertTrue(collection.isEmpty());
+    }
+
+    @Test
+    public void insertTest(){
+        collection.insert(1,"3");
+        assertEquals(3, collection.getSize());
+        assertTrue(collection.isPresent("3"));
+    }
+
+    @Test
+    public void removeTest(){
+        assertEquals(collection.remove(0),"1");
+        assertFalse(collection.isPresent("1"));
+        assertEquals(collection.getSize(),1);
+    }
+
+    @Test
+    public void isPreSentTest(){
+        assertTrue(collection.isPresent("2"));
+        assertFalse(collection.isPresent("3"));
+    }
+
 }
