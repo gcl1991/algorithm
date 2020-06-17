ae5865ba0aa77cd0cf782ea30bb36b9ab402d1ff
gcl1991 <737720583@qq.com>
2020-06-18 07:19:25 +0800

    add image description

---
+++ data_structure_and_algorithm_analysis_java3th/v04/src/test/java/content/BinarySearchTreeTest.java
@@ -0,0 +1,32 @@
+package content;
+
+import org.junit.Test;
+
+public class BinarySearchTreeTest {
+    @Test
+    public void officeTest() {
+        BinarySearchTree<Integer> t = new BinarySearchTree<>();
+        final int NUMS = 4000;
+        final int GAP = 37;
+
+        for (int i = GAP; i != 0; i = (i + GAP) % NUMS)
+            t.insert(i);
+
+        for (int i = 1; i < NUMS; i += 2)
+            t.remove(i);
+
+        if (NUMS < 40)
+            t.printTree();
+        if (t.findMin() != 2 || t.findMax() != NUMS - 2)
+            System.out.println("FindMin or FindMax error!");
+
+        for (int i = 2; i < NUMS; i += 2)
+            if (!t.contains(i))
+                System.out.println("Find error1!");
+
+        for (int i = 1; i < NUMS; i += 2) {
+            if (t.contains(i))
+                System.out.println("Find error2!");
+        }
+    }
+}
