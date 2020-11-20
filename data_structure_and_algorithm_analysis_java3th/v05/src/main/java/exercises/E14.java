9b23ccd9a7be8e38d790d02896525136f1f3f491
gcl1991 <737720583@qq.com>
2020-11-20 09:06:02 +0800

    完成练习5.14

--- data_structure_and_algorithm_analysis_java3th/v05/src/main/java/exercises/E14.java
+++ data_structure_and_algorithm_analysis_java3th/v05/src/main/java/exercises/E14.java
@@ -2,15 +2,41 @@ package exercises;
 
 public class E14 {
     public static int findSubstringStartIndex(String string, String substring) {
-        final int BASE = 31;
-        // 1 定义权重常数
+        // 1 初始化参数
+        int n = string.length();
+        int k = substring.length();
         // 2 计算待匹配字串哈希值
-        // 3 外层循环：遍历n-k+1个字串
-        //   3.1 内层顺序：
-        //      (1) 计算字串哈希
-        //      (2) 分支判断：判断哈希是否相同
-        //          1) 如果相同计算字符串是否相等
-        //              分支判断：如果相等结束函数计算，返回该字串起始索引
-        //                      如果不相等，
+        int targetHashCode = hashCode(substring);
+        // 3 for遍历n-k+1个字串
+        for (int i = 0; i < n-k+1; i++) {
+            // (1) 计算字串哈希
+            String tmpString = string.substring(i, i+k);
+            int tmpHashCode = hashCode(tmpString);
+            // （2）if/else字符串相等性判断
+            if((tmpHashCode == targetHashCode) && StringEquals(tmpString,substring)){
+                    return i;
+            }
+        }
+        return -1;
+    }
+
+    private static int hashCode(String string) {
+        int hashVal = 0;
+        int length = string.length();
+        // 1 计算字符串哈希:霍纳法则
+        for (int i = 0; i < length; i++) {
+            hashVal = 31 * hashVal + string.charAt(i);
+        }
+        return hashVal;
+    }
+
+    private static boolean StringEquals(String s1,String s2){
+        assert s1.length()==s2.length():"字符串长度必须相等";
+        for(int j=0;j<s1.length();j++){
+            if (s1.charAt(j) != s2.charAt(j)){
+                return false;
+            }
+        }
+        return true;
     }
 }
--- data_structure_and_algorithm_analysis_java3th/v05/src/test/java/exercises/E14Test.java
+++ data_structure_and_algorithm_analysis_java3th/v05/src/test/java/exercises/E14Test.java
@@ -7,8 +7,8 @@ public class E14Test {
     @Test
     public void testFindSubstringStartIndex() {
         String string = "123456789";
-        String subString = "456";
+        String subString = "567";
         int startIndex = E14.findSubstringStartIndex(string,subString);
-        assertEquals(3,startIndex);
+        assertEquals(4,startIndex);
     }
 }
